1cdbc411a2ad88c6ead5f49be1ab33d26da504d5
El Aguacate Programador <grilletjhony@gmail.com>
2021-01-13 20:27:51 -0400

    Configuring webpack for React (for didactical purposes)

--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-class ButtonCounter extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      counter: 0
-    }
-  }
-
-  increment() {
-    this.setState({ counter: this.state.counter + 1 })
-  }
-
-  render() {
-    /*return React.createElement(
-      'div',
-      {},
-      [
-        React.createElement('h2', { key: 1 }, this.state.counter),
-        React.createElement('button', {
-          key: 2,
-          onClick: () => { this.increment() }
-        }, ['Incrementar'])
-      ]
-    )*/
-    return (
-      <div>
-        <h2>{ this.state.counter }</h2>
-        <hr />
-        <button onClick={() => { this.increment() }}>Incrementar</button>
-      </div>
-    )
-  }
-}
+import Container from './components/Container'
 
 ReactDOM.render(
-  <ButtonCounter />,
+  <Container />,
   document.querySelector('#app')
 )
 
